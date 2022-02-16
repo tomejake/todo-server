4987e6fa2d3b273fbef24492cc2e12d305ca9e95
JunHyeokYu <45321171+tomejake@users.noreply.github.com>
2022-02-16 18:36:14 +0900

    commit

--- src/test/java/org/example/service/TodoServiceTest.java
+++ src/test/java/org/example/service/TodoServiceTest.java
@@ -1,10 +1,22 @@
 package org.example.service;
 
+import org.example.repository.TodoRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class TodoServiceTest {
+
+    @Mock
+    private TodoRepository todoRepository;
+    
+    @InjectMocks
+    private TodoService todoService;
 
     @Test
     void add() {
