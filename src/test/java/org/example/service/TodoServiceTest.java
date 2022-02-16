84363c155df940a7cbd3264e8a11a2c944a90f83
JunHyeokYu <45321171+tomejake@users.noreply.github.com>
2022-02-16 18:35:19 +0900

    commit

---
+++ src/test/java/org/example/service/TodoServiceTest.java
@@ -0,0 +1,16 @@
+package org.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoServiceTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void searchById() {
+    }
+}
