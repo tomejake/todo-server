03b9805392f109f4dca7dbfac3af5289615f56dc
JunHyeokYu <45321171+tomejake@users.noreply.github.com>
2022-02-16 18:19:47 +0900

    commit

--- src/main/java/org/example/controller/Controller.java
+++ src/main/java/org/example/controller/Controller.java
@@ -10,6 +10,7 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @AllArgsConstructor
@@ -34,31 +35,36 @@ public class Controller {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<TodoResponse> readOne() {
+    public ResponseEntity<TodoResponse> readOne(@PathVariable Long id) {
         System.out.println("readOne");
-        return null;
+        TodoEntity result = this.service.searchById(id);
+        return ResponseEntity.ok(new TodoResponse(result));
     }
 
     @GetMapping
     public ResponseEntity<List<TodoResponse>> readALl() {
-        System.out.println("readAll");
-        return null;
+        List<TodoEntity> list = this.service.searchAll();
+        List<TodoResponse> response = list.stream().map(TodoResponse::new).collect(Collectors.toList());
+        return ResponseEntity.ok(response);
     }
 
     @PatchMapping("{id}")
-    public ResponseEntity<TodoResponse> update() {
+    public ResponseEntity<TodoResponse> update(@PathVariable Long id, @RequestBody TodoRequest request) {
         System.out.println("update");
-        return null;
+        TodoEntity result = this.service.updateById(id, request);
+        return ResponseEntity.ok(new TodoResponse(result));
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteOne() {
+    public ResponseEntity<?> deleteOne(@PathVariable Long id) {
         System.out.println("deleteOne");
-        return null;
+        this.service.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
     public ResponseEntity<?> deleteAll() {
         System.out.println("deleteAll");
-        return null;
+        this.service.deleteAll();
+        return ResponseEntity.ok().build();
     }
 }
