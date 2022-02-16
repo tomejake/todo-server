64734f21299059ce59659019cd1148571f7f95ef
JunHyeokYu <45321171+tomejake@users.noreply.github.com>
2022-02-16 18:48:05 +0900

    add test commit

--- src/test/java/org/example/service/TodoServiceTest.java
+++ src/test/java/org/example/service/TodoServiceTest.java
@@ -1,25 +1,39 @@
 package org.example.service;
 
+import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
 import org.example.repository.TodoRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TodoServiceTest {
 
     @Mock
     private TodoRepository todoRepository;
-    
+
     @InjectMocks
     private TodoService todoService;
 
     @Test
     void add() {
+        when(this.todoRepository.save(any(TodoEntity.class)))
+                .then(AdditionalAnswers.returnsFirstArg());
+
+        TodoRequest expected = new TodoRequest();
+        expected.setTitle("Test Title");
+
+        TodoEntity actual = this.todoService.add(expected);
+
+        assertEquals(expected.getTitle(), actual.getTitle());
     }
 
     @Test
